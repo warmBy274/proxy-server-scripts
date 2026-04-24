9b87d94fe2182b028b2183b664667a6e7ebf557f
warmbu <122249231+warmBy274@users.noreply.github.com>
2026-04-24 10:01:22 +0300

    Update exit.sh

--- exit.sh
+++ exit.sh
@@ -1,1 +1,14 @@
+apt update -y && apt upgrade -y
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
 
+curl -L -o /usr/local/etc/xray/config.json https://raw.githubusercontent.com/warmBy274/proxy-server-scripts/refs/heads/main/exit.json
+clientid = xray uuid
+sed -i "s/CLIENT_ID/${clientid}/g" /usr/local/etc/xray/config.json
+read -p "Enter exit SNI: " sni
+sed -i "s/SNI/${sni}/g" /usr/local/etc/xray/config.json
+read -p "Enter exit private key: " privkey
+sed -i "s/PRIVATE_KEY/${privkey}/g" /usr/local/etc/xray/config.json
+short_id = openssl rand -hex 2
+sed -i "s/SHORT_ID/${short_id}/g" /usr/local/etc/xray/config.json
+
+systemctl restart --now xray
