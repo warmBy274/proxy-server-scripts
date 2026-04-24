d77084300e97d8c0e6fa031ed74b2a31045cd691
warmbu <122249231+warmBy274@users.noreply.github.com>
2026-04-24 13:01:52 +0300

    Update exit.sh

--- exit.sh
+++ exit.sh
@@ -1,13 +1,13 @@
 apt update -y && apt upgrade -y
 
 curl -L -o /usr/local/etc/xray/config.json https://raw.githubusercontent.com/warmBy274/proxy-server-scripts/refs/heads/main/exit.json
-clientid=xray uuid
+clientid=$(xray uuid)
 sed -i "s/CLIENT_ID/${clientid}/g" /usr/local/etc/xray/config.json
 read -p "Enter exit SNI: " sni
 sed -i "s/SNI/${sni}/g" /usr/local/etc/xray/config.json
 read -p "Enter exit private key: " privkey
 sed -i "s/PRIVATE_KEY/${privkey}/g" /usr/local/etc/xray/config.json
-shortid=openssl rand -hex 2
+shortid=$(openssl rand -hex 2)
 sed -i "s/SHORT_ID/${shortid}/g" /usr/local/etc/xray/config.json
 
 echo "Bridge client id: ${clientid}"
